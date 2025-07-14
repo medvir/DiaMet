ad29de22b96093df06372d3653676cb5ebdd2f66
iapich <pichler.ian@virology.uzh.ch>
2025-07-14 14:29:47 +0200

    added column titles to output files

--- diamet.py
+++ diamet.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.patches import Patch
 
+COLUMN_NAMES = ['qseqid', 'qlen', 'length', 'sscinames', 'sskingdoms']
+
 def run_megahit_and_diamond():
     fastq_file = "undetermined_reads.fastq.gz"
     if not os.path.isfile(fastq_file):
@@ -22,11 +24,9 @@ def run_megahit_and_diamond():
     subprocess.run(diamond_command, shell=True, check=True)
 
     if os.path.isfile(output_file):
-        df = pd.read_csv(output_file, sep='\t', header=None,
-                         names=['qseqid', 'qlen', 'length', 'sscinames', 'sskingdoms'])
-
+        df = pd.read_csv(output_file, sep='\t', header=None, names=COLUMN_NAMES)
         df_filtered = df.drop_duplicates(subset='qseqid', keep='first')
-        df_filtered.to_csv(output_file, sep='\t', header=None, index=False)
+        df_filtered.to_csv(output_file, sep='\t', header=True, index=False)
 
         print(f"Duplicates removed from {output_file}.")
 
@@ -48,9 +48,9 @@ def run_diamond_blastx():
     os.system(command)
 
 def remove_duplicate_rows(file_path):
-    df = pd.read_csv(file_path, sep='\t', header=None)
-    df = df.drop_duplicates(subset=0, keep='first')
-    df.to_csv(file_path, sep='\t', index=False, header=False)
+    df = pd.read_csv(file_path, sep='\t', header=None, names=COLUMN_NAMES)
+    df = df.drop_duplicates(subset='qseqid', keep='first')
+    df.to_csv(file_path, sep='\t', index=False, header=True)
 
 def custom_colors(entries):
     color_dict = {
@@ -62,35 +62,32 @@ def custom_colors(entries):
     return [color_dict[entry] for entry in entries]
 
 def plot_column_5(file_path, total_sequences, pdf_pages):
-    df = pd.read_csv(file_path, sep='\t', header=None)
-    counts = df[4].value_counts()
-    
+    df = pd.read_csv(file_path, sep='\t', header=None, names=COLUMN_NAMES)
+    counts = df['sskingdoms'].value_counts()
+
     desired_order = ['Eukaryota', 'Bacteria', 'Archaea', 'Viruses']
-    
-    unique_entries = sorted(counts.index.tolist(), key=lambda x: desired_order.index(x))
-
+    filtered_counts = {k: counts[k] for k in desired_order if k in counts}
+    unique_entries = list(filtered_counts.keys())
     colors = custom_colors(unique_entries)
-
+    bars = plt.bar(range(len(unique_entries)), [filtered_counts[k] for k in unique_entries], color=colors)
+    
     plt.figure(figsize=(8, 5))
-
     plt.gca().set_axisbelow(True)
     plt.grid(which='major', axis='y', linestyle='-', linewidth=1.2, alpha=0.5)
     plt.grid(which='minor', axis='y', linestyle='-', linewidth=0.5, alpha=0.2)
-
-    bars = plt.bar(range(len(unique_entries)), counts[unique_entries], color=colors)
     
     plt.ylabel('Count', fontsize=12)
     plt.yscale('log')
     plt.title('Taxonomic classification of undetermined reads', fontsize=14, fontweight='bold', loc='left', y=1.05)
-    
-    percentage_classified = len(df[0].unique()) / total_sequences * 100
+
+    percentage_classified = len(df['qseqid'].unique()) / total_sequences * 100
     subtitle = f'{percentage_classified:.2f}% of undetermined reads could be classified on protein level'
     plt.suptitle(subtitle, y=0.92, x=0.125, fontsize=10, ha='left')
-    
+
     legend_labels = [entry for entry in unique_entries]
     legend_colors = custom_colors(unique_entries)
     legend_patches = [Patch(color=color, label=label) for color, label in zip(legend_colors, legend_labels)]
-    
+
     legend = plt.legend(handles=legend_patches, loc='center left', bbox_to_anchor=(1, 0.5), fontsize='small')
 
     plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
@@ -102,10 +99,11 @@ def plot_column_5(file_path, total_sequences, pdf_pages):
     plt.gca().spines['left'].set_visible(False)
 
     plt.ylim(1, counts.max() * 1.2)
-
     pdf_pages.savefig(bbox_inches='tight')
-    
-    df[df[4] == 'Viruses'][[3]].value_counts().reset_index().to_csv('undetermined_reads_diamet_viral.csv', index=False, header=['virus_species', 'count'])
+
+    df[df['sskingdoms'] == 'Viruses'][['sscinames']].value_counts().reset_index().to_csv(
+        'undetermined_reads_diamet_viral.csv', index=False, header=['virus_species', 'count']
+    )
 
 if __name__ == "__main__":
     pdf_path = 'undetermined_reads_diamet.pdf'
